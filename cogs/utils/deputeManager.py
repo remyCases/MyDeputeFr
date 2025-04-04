b4984c70a8885787131e12756e38329ea6cbc28e
remyCases <remy.cases@gmail.com>
2025-04-04 15:18:17 +0200

    [Minor] fixing header

--- cogs/utils/deputeManager.py
+++ cogs/utils/deputeManager.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2025 Rémy Cases
 # See LICENSE file for extended copyright information.
-# This file is part of StatisticalAgreement project from https://github.com/remyCases/StatisticalAgreement.
+# This file is part of MyDeputeFr project from https://github.com/remyCases/MyDeputeFr.
 
 import os
 import json
