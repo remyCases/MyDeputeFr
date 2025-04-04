f195036def147b6062372d27bf4a6f4f111f8d04
remyCases <remy.cases@gmail.com>
2025-04-04 23:17:18 +0200

    [Major] download module using the same logger as the bot

--- download/download.py
+++ download/download.py
@@ -6,21 +6,12 @@ import os
 import requests
 import zipfile
 import shutil
-import logging
+from logging import Logger
 import time
 import schedule
 from dotenv import load_dotenv
 
 load_dotenv()
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler("bot_updater.log"),
-        logging.StreamHandler()
-    ]
-)
 
 # Configuration
 URL_DOWNLOAD = os.getenv("URL_DOWNLOAD")
@@ -28,14 +19,14 @@ TEMP_FOLDER = os.getenv("TEMP_FOLDER")
 DATA_FOLDER = os.getenv("SCRUTINS_FOLDER")
 UPDATE_HOUR = os.getenv("UPDATE_HOUR")
 
-def download_file(url: str, des_folder: str) -> str | None:
+def download_file(log: Logger, url: str, des_folder: str) -> str | None:
     """TODO"""
     try:
         if not os.path.exists(des_folder):
             os.makedirs(des_folder)
             
         file_name = os.path.join(des_folder, "data.zip")
-        logging.info(f"Download from {url}")
+        log.info(f"Download from {url}")
         
         response = requests.get(url, stream=True)
         response.raise_for_status()
@@ -44,14 +35,14 @@ def download_file(url: str, des_folder: str) -> str | None:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
                 
-        logging.info(f"Download done {file_name}")
+        log.info(f"Download done {file_name}")
         return file_name
         
     except Exception as e:
-        logging.error(f"Error during download: {str(e)}")
+        log.error(f"Error during download: {str(e)}")
         return None
 
-def unzip_file(path: str, des_folder: str) -> str | None:
+def unzip_file(log: Logger, path: str, des_folder: str) -> str | None:
     """TODO"""
     try:
         if not os.path.exists(des_folder):
@@ -62,21 +53,21 @@ def unzip_file(path: str, des_folder: str) -> str | None:
             shutil.rmtree(temp)
         os.makedirs(temp)
         
-        logging.info(f"Unzipping file {path}")
+        log.info(f"Unzipping file {path}")
         with zipfile.ZipFile(path, "r") as zip_ref:
             zip_ref.extractall(temp)
             
-        logging.info(f"Unzipping done {temp}")
+        log.info(f"Unzipping done {temp}")
         return temp
         
     except Exception as e:
-        logging.error(f"Error during unzipping: {str(e)}")
+        log.error(f"Error during unzipping: {str(e)}")
         return None
 
-def moving_folder(src_folder: str, des_folder: str):
+def moving_folder(log: Logger, src_folder: str, des_folder: str):
     """TODO"""
     try:
-        logging.info(f"Moving all files from {src_folder}")
+        log.info(f"Moving all files from {src_folder}")
         all_file = []
         
         # Find all files to be moved
@@ -85,7 +76,7 @@ def moving_folder(src_folder: str, des_folder: str):
                 path = os.path.join(root, f)
                 all_file.append(path)
         
-        logging.info(f"Moving in {des_folder}")
+        log.info(f"Moving in {des_folder}")
         if not os.path.exists(des_folder):
             os.makedirs(des_folder)
 
@@ -101,63 +92,60 @@ def moving_folder(src_folder: str, des_folder: str):
             # copy new file
             shutil.copy2(src_path, des_path)
                 
-        logging.info(f"Moving done in {des_folder}")
+        log.info(f"Moving done in {des_folder}")
         return True
         
     except Exception as e:
-        logging.error(f"Erorr during renaming : {str(e)}")
+        log.error(f"Erorr during renaming : {str(e)}")
         return False
 
-def clean(zip_file: str, temp: str | None=None) -> None:
+def clean(log: Logger, zip_file: str, temp: str | None=None) -> None:
     """TODO"""
     try:
         if os.path.exists(zip_file):
             os.remove(zip_file)
-            logging.info(f"Deleted file: {zip_file}")
+            log.info(f"Deleted file: {zip_file}")
             
         if temp and os.path.exists(temp):
             shutil.rmtree(temp)
-            logging.info(f"Deleted folder: {temp}")
+            log.info(f"Deleted folder: {temp}")
             
     except Exception as e:
-        logging.error(f"Error during cleaning: {str(e)}")
+        log.error(f"Error during cleaning: {str(e)}")
 
-def update() -> bool:
+def update(log: Logger) -> bool:
     """TODO"""
-    logging.info("=== Update starting ===")
+    log.info("=== Update starting ===")
     
-    zip_file = download_file(URL_DOWNLOAD, TEMP_FOLDER)
+    zip_file = download_file(log, URL_DOWNLOAD, TEMP_FOLDER)
     if not zip_file:
-        logging.error("Update failed: download failed")
+        log.error("Update failed: download failed")
         return False
     
-    folder = unzip_file(zip_file, TEMP_FOLDER)
+    folder = unzip_file(log, zip_file, TEMP_FOLDER)
     if not folder:
-        clean(zip_file)
-        logging.error("Update failed : unzipping failed")
+        clean(log, zip_file)
+        log.error("Update failed : unzipping failed")
         return False
     
-    success = moving_folder(folder, DATA_FOLDER)
-    clean(zip_file, folder)
+    success = moving_folder(log, folder, DATA_FOLDER)
+    clean(log, zip_file, folder)
     
     if success:
-        logging.info("=== Update success ===")
+        log.info("=== Update success ===")
     else:
-        logging.error("=== Update failed ===")
+        log.error("=== Update failed ===")
     
     return success
 
-def start_planning():
+def start_planning(log: Logger):
     """TODO"""
     schedule.every().day.at(UPDATE_HOUR).do(update)
-    logging.info(f"Update planed at {UPDATE_HOUR}")
+    log.info(f"Update planed at {UPDATE_HOUR}")
     
-    logging.info("First update...")
-    update()
+    log.info("First update...")
+    update(log)
 
     while True:
         schedule.run_pending()
         time.sleep(900)  # Check every 15min
-
-if __name__ == "__main__":
-    start_planning()
--- main.py
+++ main.py
@@ -6,64 +6,22 @@ Description:
 Version: 6.3.0
 """
 
-import logging
 import os
 import platform
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 from discord.ext.commands import Context
 from dotenv import load_dotenv
+
+from logger import logger
+from download import download
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-class LoggingFormatter(logging.Formatter):
-    black = "\x1b[30m"
-    red = "\x1b[31m"
-    green = "\x1b[32m"
-    yellow = "\x1b[33m"
-    blue = "\x1b[34m"
-    gray = "\x1b[38m"
-    reset = "\x1b[0m"
-    bold = "\x1b[1m"
-
-    COLORS = {
-        logging.DEBUG: gray + bold,
-        logging.INFO: blue + bold,
-        logging.WARNING: yellow + bold,
-        logging.ERROR: red,
-        logging.CRITICAL: red + bold,
-    }
-
-    def format(self, record):
-        log_color = self.COLORS[record.levelno]
-        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
-        format = format.replace("(black)", self.black + self.bold)
-        format = format.replace("(reset)", self.reset)
-        format = format.replace("(levelcolor)", log_color)
-        format = format.replace("(green)", self.green + self.bold)
-        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
-        return formatter.format(record)
-
-
-logger = logging.getLogger("discord_bot")
-logger.setLevel(logging.INFO)
-
-# Console handler
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(LoggingFormatter())
-# File handler
-file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-file_handler_formatter = logging.Formatter(
-    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
-)
-file_handler.setFormatter(file_handler_formatter)
-
-# Add the handlers
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+logger = logger.init_logger("discord.log")
 
 class DiscordBot(commands.Bot):
     def __init__(self) -> None:
@@ -197,5 +155,6 @@ class DiscordBot(commands.Bot):
         else:
             raise error
 
+download.start_planning(log=logger)
 bot = DiscordBot()
 bot.run(os.getenv("TOKEN"))
