6376d349c0e00feb46995a147b8a6c0e35635a6a
remyCases <remy.cases@gmail.com>
2025-04-04 22:16:48 +0200

    [Minor] adding missing header

--- download/download.py
+++ download/download.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2025 Rémy Cases
+# See LICENSE file for extended copyright information.
+# This file is part of MyDeputeFr project from https://github.com/remyCases/MyDeputeFr.
+
 import os
 import requests
 import zipfile
