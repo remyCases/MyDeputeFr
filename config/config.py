19c693c2767af0d39e137b1795e84f448926d100
remyCases <remy.cases@gmail.com>
2025-04-07 17:56:22 +0200

    [Minor] adding new env variable for a tcp connection, fixing a trypo in update at launch

--- config/config.py
+++ config/config.py
@@ -14,7 +14,7 @@ load_dotenv()
 class MissingEnvException(Exception):
     """Exception raised when an environment variable is missing."""
 
-__HIDE_VAL_IN_LOG = ["DISCORD_TOKEN"]  # List of values to hide in the log
+__HIDE_VAL_IN_LOG = ["DISCORD_TOKEN", "TCP_TOKEN"]  # List of values to hide in the log
 
 def show_config(module: ModuleType, logger: Logger):
     """Displays the attributes of a module, ignoring certain sensitive values."""
@@ -53,7 +53,7 @@ UPDATE_URL_DOWNLOAD_ACTEUR_ORGANE = __load_env(
     "https://data.assemblee-nationale.fr/static/openData/repository/17/amo/deputes_actifs_mandats_actifs_organes/"
     "AMO10_deputes_actifs_mandats_actifs_organes.json.zip") # URL to update acteur et organe
 UPDATE_HOUR = __load_env("UPDATE_HOUR", lambda: "08:00:00")  # Default update time
-UPDATE_AT_LAUNCH = __load_env("UPDATE_AT.LAUNCH", lambda: "TRUE").upper() in ('TRUE', '1', 'T')  # Enable updates at launch
+UPDATE_AT_LAUNCH = __load_env("UPDATE_AT_LAUNCH", lambda: "TRUE").upper() in ('TRUE', '1', 'T')  # Enable updates at launch
 UPDATE_PROGRESS_SECOND = int(__load_env("UPDATE_DOWNLOAD_PROGRESS_SECOND", lambda: "2")) # Download progress update in second, if 0 is disabled
 
 # Folders
@@ -64,3 +64,8 @@ SCRUTINS_FOLDER = __load_env("SCRUTINS_FOLDER", lambda: "data/scrutins")  # Path
 # Logs
 LOG_PATH = __load_env("LOG_PATH", lambda: "discord.log")  # Path to the log file
 LOG_LEVEL = __load_env("LOG_LEVEL", lambda: "INFO").upper()  # Logging level (INFO, DEBUG...)
+
+# TCP connection
+TCP_TOKEN = __load_env_required("TCP_TOKEN")
+TCP_SERVER_HOST = __load_env("TCP_SERVER_HOST", lambda: "127.0.0.1")
+TCP_SERVER_PORT = __load_env("TCP_SERVER_PORT", lambda: "8733")
