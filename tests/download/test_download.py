bfcc4a96b853fc598954e43c1d372dd65d84949c
remyCases <remy.cases@gmail.com>
2025-04-10 00:58:38 +0200

    [Major] updating downloads test for the async functions, adding a new test differentiating invalidurl and connextion error, adding a client response error test

--- tests/download/test_download.py
+++ tests/download/test_download.py
@@ -6,36 +6,69 @@ import hashlib
 import os
 import tempfile
 
+import aiohttp
 import pytest
-import requests
 
-from download.download import download_file
-from tests.common import mock_log
+from download.core import download_file_async
 
 
-def test_download(mock_log):
+@pytest.mark.asyncio
+async def test_download(mock_log):
+    """Test a valid download"""
     url = "https://proof.ovh.net/files/1Mb.dat"
     with tempfile.TemporaryDirectory() as download_temp:
         file_path = os.path.join(download_temp, "data.dat")
-        download_file(mock_log, url, file_path)
+        await download_file_async(mock_log, url, file_path)
         assert os.path.getsize(file_path) == 1048576, "Invalid size"
-        with open(file_path,'rb') as f:
-            assert hashlib.md5(f.read()).hexdigest() == "d5eefcccdb834958512bed157d01f3a7", "Invalid md5 hash"
+        with open(file_path, "rb") as f:
+            assert hashlib.md5(f.read()).hexdigest() == "d5eefcccdb834958512bed157d01f3a7", \
+                "Invalid md5 hash"
 
         # Check logs
-        mock_log.info.assert_any_call(f"Downloading {url} to {file_path}")
+        mock_log.info.assert_any_call("Downloading %s to %s", url, file_path)
         mock_log.info.assert_any_call("Download done")
 
 
-def test_download_invalid_url(mock_log):
+@pytest.mark.asyncio
+async def test_download_invalid_url(mock_log):
+    """Test a download using an invalid url"""
+    url = "https:////proof.ovh.net/files/1Mb.dat"
+    with tempfile.TemporaryDirectory() as download_temp:
+        file_path = os.path.join(download_temp, "data.dat")
+        with pytest.raises(aiohttp.InvalidURL):
+            await download_file_async(mock_log, url, file_path)
+
+            # Check logs
+            mock_log.info.assert_any_call("Connection error from %s", url)
+
+@pytest.mark.asyncio
+async def test_download_client_connection_error(mock_log):
+    """Test a download using a valid url but from an nonexistent domaine"""
     url = "https://proof.ovh.error/files/1Mb.dat"
     with tempfile.TemporaryDirectory() as download_temp:
         file_path = os.path.join(download_temp, "data.dat")
-        with pytest.raises(requests.exceptions.ConnectionError):
-            download_file(mock_log, url, file_path)
+        with pytest.raises(aiohttp.ClientConnectionError):
+            await download_file_async(mock_log, url, file_path)
 
-def test_download_invalid_path(mock_log):
+            # Check logs
+            mock_log.info.assert_any_call("Connection error from %s", url)
+
+@pytest.mark.asyncio
+async def test_download_client_response_error(mock_log):
+    """Test a download using a valid url but for an nonexistent page"""
+    url = "https://proof.ovh.net/files/invalid"
+    with tempfile.TemporaryDirectory() as download_temp:
+        file_path = os.path.join(download_temp, "data.dat")
+        with pytest.raises(aiohttp.ClientResponseError):
+            await download_file_async(mock_log, url, file_path)
+
+            # Check logs
+            mock_log.info.assert_any_call("Invalid response from %s", url)
+
+@pytest.mark.asyncio
+async def test_download_invalid_path(mock_log):
+    """Test to download in a non existent folder"""
     url = "https://proof.ovh.net/files/1Mb.dat"
     file_path = os.path.join("/do/not/exist", "data.dat")
     with pytest.raises(FileNotFoundError) :
-        download_file(mock_log, url, file_path)
+        await download_file_async(mock_log, url, file_path)
