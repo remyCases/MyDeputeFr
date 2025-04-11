35bfbf7aaec5c49631416db55c84d69e703e6ba3
remyCases <remy.cases@gmail.com>
2025-04-11 20:13:46 +0200

    [Minor] updating cogManager since allow_during_update is not necessary anymore and part of the logic is moved in the new decorator

--- utils/cogManager.py
+++ utils/cogManager.py
@@ -3,48 +3,34 @@
 # This file is part of MyDeputeFr project from https://github.com/remyCases/MyDeputeFr.
 
 from functools import wraps
-from typing import Any, Callable, Self, TypeVar, cast
-import inspect
+from typing import Self, cast
 from discord.ext import commands
+from discord.ext.commands import Context
+from discord.ext.commands.hybrid import T
 
-# Generic type for typing commands
-T = TypeVar('T', bound=Callable[..., Any])
+from utils.botManager import DiscordBot
 
-def not_updating() -> Callable[[T], T]:
+class ProtectedCog(commands.Cog):
+    """Class with all commands protected during updates"""
+    def __init__(self: Self, bot: DiscordBot) -> None:
+        self.bot: DiscordBot = bot
+        if not hasattr(bot, "update_lock"):
+            raise commands.ExtensionError(
+                "Missing lock to handle blocking commands during update.", 
+                name="ProtectedCog"
+            )
+
+def not_updating():
     """Decorator to ensure commands will not be executed during an update"""
     def decorator(func: T) -> T:
         @wraps(func)
-        async def wrapper(cog, context, *args, **kwargs):
+        async def wrapper(cog: ProtectedCog, context: Context, *args, **kwargs):
             if cog.bot.update_lock.locked() or cog.bot.is_updating:
                 await context.send(
                     "Le bot est en cours de mise à jour. Service temporairement indisponible."
                 )
                 return None
 
-            # using lock for security
-            async with cog.bot.update_lock:
-                return await func(cog, context, *args, **kwargs)
-
+            return await func(cog, context, *args, **kwargs)
         return cast(T, wrapper)
     return decorator
-
-def allow_during_update(func):
-    """Decorator to bypass the protection during updates"""
-    func.allow_during_update = True
-    return func
-
-class ProtectedDuringUpdateCog(commands.Cog):
-    """Class with all commands protected during updates"""
-    def __init__(self: Self, bot) -> None:
-        self.bot = bot
-        if not hasattr(bot, "update_lock"):
-            raise commands.ExtensionError(
-                "Missing lock to handle blocking commands during update.", 
-                name="ProtectedDuringUpdateCog"
-            )
-
-        # Apply not_updating decorator to all methods that are a command
-        for name, method in inspect.getmembers(self, predicate=inspect.ismethod):
-            if isinstance(method, commands.Command) or hasattr(method, "__command_flag__"):
-                if not getattr(method, "allow_during_update", False):
-                    setattr(self, name, not_updating()(method))
